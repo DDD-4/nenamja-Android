e6ac3aa1fafac0ac58316201417cecaad3bdcaa2
Jason <ironelder@gmail.com>
2020-10-09 15:57:48 +0900

    서버에서 사용하는 쿼리 적용

--- app/src/main/java/com/nenamja/volunteer/data/remote/api/NenamjaServiceApi.kt
+++ app/src/main/java/com/nenamja/volunteer/data/remote/api/NenamjaServiceApi.kt
@@ -1,20 +1,16 @@
 package com.nenamja.volunteer.data.remote.api
 
 import com.nenamja.volunteer.BuildConfig
+import com.nenamja.volunteer.data.remote.model.VolunteerListModel
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NenamjaServiceApi {
-    @GET("getVolProgrmList")
+    @GET("/mainVolList")
     fun getVolunteerList(
-        @Query("query") serviceKey: String = BuildConfig.AUTH_KEY,
         @Query("query") pageNo: Int? = 1,
-        @Query("query") numOfRows: Int? = 10,
-        @Query("query") sido: String?,
-        @Query("query") pgm: String?,
-        @Query("query") org: String?,
-        @Query("query") sdate: String?,
-        @Query("query") edate: String?
-    ): Single<List<Any>>
+        @Query("query") rowNum: Int? = 10,
+        @Query("query") todayDate: String?
+    ): Single<VolunteerListModel>
 }
