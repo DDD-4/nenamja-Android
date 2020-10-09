7b326e27f55f16d98eccbaadae980aafc7fcafda
Jason <ironelder@gmail.com>
2020-10-09 15:58:25 +0900

    서버에서 데이터 받아오기 위한 모델 추가

---
+++ app/src/main/java/com/nenamja/volunteer/data/remote/model/VolunteerContent.kt
@@ -0,0 +1,10 @@
+package com.nenamja.volunteer.data.remote.model
+
+data class VolunteerContent(
+    val KEY1: Int,
+    val ORGANNM: String,
+    val PGMNM: String,
+    val PRICE: Int,
+    val SDATE: String,
+    val TARGET: String
+)
---
+++ app/src/main/java/com/nenamja/volunteer/data/remote/model/VolunteerListModel.kt
@@ -0,0 +1,6 @@
+package com.nenamja.volunteer.data.remote.model
+
+data class VolunteerListModel(
+    val contents: List<VolunteerContent>,
+    val result: String
+)
